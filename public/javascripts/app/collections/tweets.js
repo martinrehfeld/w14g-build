cfbbd204f70914a9a9b8086e810044f884fe89ea
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-22 23:12:46 +0100

    keep Tweets collection sorted by descending id

--- public/javascripts/app/collections/tweets.js
+++ public/javascripts/app/collections/tweets.js
@@ -1,4 +1,8 @@
 App.Collections.Tweets = Backbone.Collection.extend({
   model: Tweet,
-  url: '/tweets'
+  url: '/tweets' // not used for collection fetch!
 });
+
+App.Collections.Tweets.prototype.comparator = function (tweet) {
+  return -(tweet.get('id'));
+};
