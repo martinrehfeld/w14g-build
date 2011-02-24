9ed38e4931d1d700c4c68733182f2a1e330a58b5
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-24 01:47:50 +0100

    provide App.escapeHTML helper

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,5 +1,12 @@
 var App = {
   Views: {},
   Controllers: {},
-  Collections: {}
+  Collections: {},
+
+  // Helper function to escape a string for HTML rendering.
+  // taken from backbone.js where it sadly is only available
+  // for Backbone.Model attributes
+  escapeHTML: function (string) {
+    return string.replace(/&(?!\w+;)/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+  }
 };
