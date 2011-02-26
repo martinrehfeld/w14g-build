e0076f9187108edeed7d0ebefdfaebf50de2936a
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-27 00:52:55 +0100

    use $.append once instead of $.html in render

--- public/javascripts/app/views/tweets.js
+++ public/javascripts/app/views/tweets.js
@@ -3,11 +3,11 @@ App.Views.Tweets = Backbone.View.extend({
     _.bindAll(this, 'render');
     this.options.parent.bind('change', this.render);
     this.collection.bind('filterchange', this.render);
+    $('#app .tweets').append(this.el);
   },
 
   render: function () {
-    $(this.el).html(JST.tweets_collection({ collection: this.collection.visible() }));
-    $('#app .tweets').html(this.el);
+    $(this.el).html(JST.tweets_collection({ collection: this.collection }));
     return this;
   }
 });
--- public/javascripts/app/views/word_cloud.js
+++ public/javascripts/app/views/word_cloud.js
@@ -2,14 +2,13 @@ App.Views.WordCloud = Backbone.View.extend({
   initialize: function () {
     _.bindAll(this, 'render');
     this.options.parent.bind('change', this.render);
+    $('#app .word-cloud').append(this.el);
   },
 
   render: function () {
     $(this.el)
       .html(JST.tweets_wordcloud({ collection: this.model.topEntries(75) }))
       .children('a').tagcloud();
-
-    $('#app .word-cloud').html(this.el);
 
     return this;
   }
