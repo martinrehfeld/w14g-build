ff3b0d6b612d49df7a85dcb0972abbac95108f29
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-02-27 01:24:42 +0100

    memoize Tweet#analysedWords

--- public/javascripts/app/models/tweet.js
+++ public/javascripts/app/models/tweet.js
@@ -1,9 +1,18 @@
 App.Models.Tweet = Backbone.Model.extend({
   analysedWords: function () {
+    var cache = App.Models.Tweet.cache;
+    var id = this.get('id');
+
     // just do some simple processing for now,
     // an actual stemmer should be used instead!
-    return _.map(this.get('text').replace(/[!"()+*?&.,;:]/g, ' ').split(/\s+/), function (word) {
-      return word.toLocaleLowerCase();
-    });
+    if (!cache[id]) {
+      cache[id] = _.map(this.get('text').replace(/[!"()+*?&.,;:]/g, ' ').split(/\s+/), function (word) {
+        return word.toLocaleLowerCase();
+      });
+    }
+    return cache[id];
   }
+},{
+  // class properties
+  cache: {}
 });
