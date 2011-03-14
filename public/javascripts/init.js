544e965ef91e190d7b7b99930bf96411d6b4e525
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-03-14 15:00:15 +0100

    use explicit reference to global context

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,10 +1,8 @@
 ({
   init: function (global) {
-    var self = this;
-
     // HTML5 fix for older browsers
     'article aside footer header nav section time'.replace(/\w+/g, function (n) {
-      document.createElement(n);
+      global.document.createElement(n);
     });
 
     $.fn.tagcloud.defaults = {
