martinrehfeld/w14g-build

commit 2a97815b81cc1de16b46e4b2e5edea68cc3540ee

somewhat more consistent code style

app/collection/tweets.coffee
@@ -15,12 +15,16 @@ class App.Collections.Tweets extends Backbone.Collection
     @refresh [], options
 
   fetch: (report) ->
-    page = 1
-    screenName = report.get('screenName')
-    baseUrl = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=' +
+    page       = 1
+    screenName = report.get 'screenName'
+    baseUrl    = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name = ' +
               encodeURIComponent(screenName) +
               '&include_rts=1&include_entities=1&trim_user=1&callback=?&count=200&page='
 
+    # TODO: inform user about errors
+    #       - 400 Bad Request will be returned when you are over the rate limit
+    #       - 401 Unauthorized will be given when the feed is protected
+    #       - 404 Not Found will be given when the screenname is unknown
     fetchNext = =>
       $.getJSON baseUrl + page, (data) =>
         if data.length > 0 && report.get('screenName') == screenName
@@ -32,8 +36,8 @@ class App.Collections.Tweets extends Backbone.Collection
     setTimeout fetchNext, 0
 
   tweetAdded: (newTweet) ->
-    wordMap = @wordMap
-    currentFilter = @currentFilter
+    wordMap              = @wordMap
+    currentFilter        = @currentFilter
     matchesCurrentFilter = false
 
     if !@isFiltered()
@@ -57,25 +61,18 @@ class App.Collections.Tweets extends Backbone.Collection
     options ?= {}
 
     @currentFilter = null
-    @each (tweet) ->
-      tweet.set visible: true
-
-    if !options.silent
-      @trigger 'filterchange'
+    @each (tweet) -> tweet.set visible: true
+    @trigger 'filterchange' if !options.silent
 
   filterByWord: (word) ->
-    wordMap = @wordMap
-    if !wordMap[word]
-      wordMap[word] = []
+    wordMap        = @wordMap
+    wordMap[word]  = [] if !wordMap[word]
     @visibleModels = wordMap[word]
     @currentFilter = word
 
-    @each (tweet) ->
-      tweet.set visible: false
-
+    @each (tweet) -> tweet.set visible: false
     for tweet in @visibleModels
       tweet.set visible: true
-
     @trigger 'filterchange'
 
   visible: ->

app/app.coffee
@@ -1,6 +1,6 @@
 @App =
-  Models: {}
-  Views: {}
+  Models:      {}
+  Views:       {}
   Controllers: {}
   Collections: {}
 

app/controller/report.coffee
@@ -1,12 +1,12 @@
 class App.Controllers.Report extends Backbone.Controller
 
   routes:
-    "": "index"
-    ":screenName": "show"
-    ":screenName/:word": "filter"
+    ""                  : "index"
+    ":screenName"       : "show"
+    ":screenName/:word" : "filter"
 
   initialize: ->
-    @model = new App.Models.Report
+    @model    = new App.Models.Report
     @showView = new App.Views.Show(model: @model).render()
 
   index: ->
@@ -18,4 +18,4 @@ class App.Controllers.Report extends Backbone.Controller
 
   filter: (screenName, word) ->
     @model.set screenName: screenName
-    @model.filterByWord decodeURIComponent word
+    @model.filterByWord decodeURIComponent(word)

app/model/cloud.coffee
@@ -18,18 +18,17 @@ class App.Models.Cloud extends Backbone.Model
 
   entries: ->
     filterRegex = new RegExp "^#{@get('filter') ? ''}"
-    result = {}
+    result      = {}
 
     for entry, frequency of @get('frequencyMap') when filterRegex.test entry
       result[entry] = frequency
 
     result
 
-
   topEntries: (count) ->
-    entries = @entries()
+    entries                   = @entries()
     lowestAcceptableFrequency = _.last _.first _.sortBy(_.values(entries), (frequency) -> -frequency), count
-    result = {}
+    result                    = {}
 
     for entry, frequency of entries when frequency >= lowestAcceptableFrequency
       break if count < 1

app/model/report.coffee
@@ -8,14 +8,14 @@ class App.Models.Report extends Backbone.Model
     @bind 'change:screenName', @newScreenName
 
   newScreenName: =>
-    screenName = @get 'screenName'
     tweets     = @get 'tweets'
+    screenName = @get 'screenName'
     wordCloud  = @get 'wordCloud'
 
     tweets.clear()
     wordCloud.clear()
     @trigger 'change'
-    tweets.fetch(this)
+    tweets.fetch this
 
   filterByWord: (word) ->
     @get('tweets').filterByWord word

app/model/tweet.coffee
@@ -6,8 +6,7 @@ class App.Models.Tweet extends Backbone.Model
 
   analyze: ->
     words = @extractEntities()
-    @set
-      analysedWords: words.concat @extractWords @get 'textWithoutEntities'
+    @set analysedWords: words.concat @extractWords(@get 'textWithoutEntities')
 
   extractEntities: ->
     entities    = @get 'entities'

app/view/index.coffee
@@ -9,6 +9,6 @@ class App.Views.Index extends Backbone.View
 
   render: =>
     $(@el).html JST.report_form model: @model
-    $('#app #form').empty().append(@el)
+    $('#app #form').empty().append @el
     $(@el).find('input').first().focus()
     @

app/view/show.coffee
@@ -9,5 +9,5 @@ class App.Views.Show extends Backbone.View
     @tweetsView        = new App.Views.Tweets(parent: @model, collection: tweets).render()
 
   render: =>
-    $('#app #report').empty().append(@wordCloudView.el, @tweetsFilterView.el, @tweetsView.el)
+    $('#app #report').empty().append @wordCloudView.el, @tweetsFilterView.el, @tweetsView.el
     @

commit 41e47f06bdbe3d2235e3b5a5470e14741b258add

fix Twitter API URL
* bug was introduced in 2a97815b81cc1de

app/collection/tweets.coffee
@@ -17,7 +17,7 @@ class App.Collections.Tweets extends Backbone.Collection
   fetch: (report) ->
     page       = 1
     screenName = report.get 'screenName'
-    baseUrl    = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name = ' +
+    baseUrl    = 'http://api.twitter.com/1/statuses/user_timeline.json?screen_name=' +
               encodeURIComponent(screenName) +
               '&include_rts=1&include_entities=1&trim_user=1&callback=?&count=200&page='
 