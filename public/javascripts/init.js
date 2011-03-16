78a1a3c3b3b94807838527445296c75b1ef03536
Martin Rehfeld <martin.rehfeld@glnetworks.de>
2011-03-16 17:47:29 +0100

    add tweets per day visualization

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,5 +1,8 @@
 ({
   init: function (global) {
+    google.load('visualization', '1', {packages:['corechart']});
+    google.setOnLoadCallback(this.start);
+
     // HTML5 fix for older browsers
     'article aside footer header nav section time'.replace(/\w+/g, function (n) {
       global.document.createElement(n);
@@ -9,8 +12,10 @@
       size: {start: 13, end: 28, unit: "px"},
       color: {start: '#aaaaaa', end: '#0388a6'}
     };
+  },
 
-    // init App when DOM ready
+  start: function () {
+    // start App when DOM ready
     $(function() {
       new App.Controllers.Report();
       Backbone.history.start();
